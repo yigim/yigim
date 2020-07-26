daeddbb1b76f25c6ae1d6567edfadfddb5129986
geongun20 <leesg9@snu.ac.kr>
2020-07-26 21:00:02 +0900

    set server environment

---
+++ README.md
@@ -0,0 +1,3 @@
+### todo
+
+set project name
---
+++ packages/yigim-server/src/http/hello.ts
@@ -0,0 +1,9 @@
+import { APIGatewayProxyHandler } from 'aws-lambda';
+export const handler: APIGatewayProxyHandler = async (_event, _context) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: 'Hello World!',
+    }),
+  };
+};
