396fa084ad380fbbcf58cb8d84fc0b18d9fe44c1
kyungwon-lee <kw3753@naver.com>
2020-09-06 23:44:00 +0900

    Home css/js responsive

--- packages/yigim-client/src/components/userinfo.js
+++ packages/yigim-client/src/components/userinfo.js
@@ -1,66 +1,66 @@
-import React, { Component } from "react";
-import {BrowserRouter as Router, Route, NavLink } from "react-router-dom";
-import ProbMaking from "./probmaking";
-import './userinfo.css'
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
+import ProbMaking from './probmaking';
+import './userinfo.css';
 
-class UserInfo extends Component{
-  constructor(props){
+class UserInfo extends Component {
+  constructor(props) {
     super(props);
-    this.state={
+    this.state = {
       name: '',
-    }
+    };
   }
-  render(){
-    return(
-        <article>
-          <form onSubmit={(e)=>{
-              e.preventDefault();
-              this.props.history.push('/prob-make-ready/'+this.state.name+'/0');
-              }}>
-            <div className="Desktop10">
-            <h1>2020학년도 너 나 알아? 친구 영역</h1>
-            <h2>친구 적성평가</h2>
-            <p className="Problemtype">홀수형</p>
-            <p className="Inputname1">성명</p>
-            <input className="Inputname2" type="text" name="username" placeholder="이름을 입력해주세요."
-            onChange={(e)=>{
-              e.preventDefault();
-              this.setState({
-                name: e.target.value
-              })
-            }}></input>
-            <p className="Inputstudentid1">학번</p>
-            <input className="Inputstudentid2" type="text"></input>
-            <p className="Inputgender1">성별</p>
-            <input type="button" className="Inputgender2" value="남자"></input>
-            <input type="button" className="Inputgender3" value="여자"></input>
-            <p className="Caution">※시험이 시작되기 전까지 표지를 넘기지 마시오.</p>
-            <input type="submit" className="button1" value="출제하기"
-            ></input>
-            <NavLink to="/">
-              <input type="button" className="button2" value="뒤로"></input>
-            </NavLink>
+  render() {
+    return (
+      <article>
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            this.props.history.push('/prob-make-ready/' + this.state.name + '/0');
+          }}
+        >
+          <div className="Home_Container">
+            <div className="Sub_title">2020학년도 너 나 알까? 친구 영역</div>
+            <div className="Main_title">친구 적성평가</div>
+            <div className="Problemtype">홀수형</div>
+            <div className="Inputname">
+              <label>성명 </label>
+              <input
+                type="text"
+                name="username"
+                placeholder="이름을 입력해주세요."
+                onChange={(e) => {
+                  e.preventDefault();
+                  this.setState({
+                    name: e.target.value,
+                  });
+                }}
+              ></input>
+            </div>
+
+            <div className="Inputstudentid">
+              <label>학번 </label>
+              <input className="Inputstudentid2" type="text"></input>
+            </div>
+
+            <div className="Inputgender">
+              <label>성별 </label>
+              <input type="button" className="Inputgender2" value="남자"></input>
+              <input type="button" className="Inputgender3" value="여자"></input>
+            </div>
+
+            <div className="Problemmakestart-1">
+              <input
+                type="submit"
+                className="button1"
+                value="나만의 문제 출제하러 가기! GO!"
+              ></input>
+            </div>
+            <div className="Caution">※시험이 시작되기 전까지 표지를 넘기지 마시오.</div>
           </div>
-            
-
-
-          </form>
+        </form>
       </article>
     );
   }
 }
 export default UserInfo;
-
-// import React, { Component } from "react";
-// import { BrowserRouter as Router, Route} from "react-router-dom";
-
-// const UserInfo = ({history}) => {
-//     return(
-//       <div>
-//         <button onClick={()=>{history.push('/')}}>
-//           버어어튼
-//           </button>
-//       </div>
-//     );
-// }
-// export default UserInfo;
